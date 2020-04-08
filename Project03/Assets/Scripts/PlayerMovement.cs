f656be3ae63dc3a823e6b3b6eb71b6e9f8ad67f1
Andy Anguiano <aanguiano@chapman.edu>
2020-04-08 05:19:33 +0000

    Watched Videos

---
+++ Project03/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 25f;
+    public bool hasJumpPotion = false;
+    public bool hasSpeedPotion = false;
+    public int potionModAmount = 0;
+
+    private float potionTimeMax = 10f;
+    private float potionTimeCur = 0f;
+
+    float horizontalMove = 0f;
+
+    bool jumpFlag = false;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if(jumpFlag)
+        {
+            jumpFlag = false;
+            animator.SetBool("IsJumping", true);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    public void onLanding()
+    {
+        animator.SetBool("IsJumping", false);
+        jump = false;
+    }
+
+    void FixedUpdate()
+    {
+        if(hasJumpPotion && potionTimeCur < potionTimeMax)
+        {
+            controller.m_JumpForceMod = potionModAmount;
+            potionTimeCur += Time.fixedDeltaTime;
+        }
+        else
+        {
+            potionTimeCur = 0f;
+            controller.m_JumpForceMod = 0;
+            hasJumpPotion = false;
+        }
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+
+        if(jump)
+        {
+            jumpFlag = true;
+        }
+    }
+}
